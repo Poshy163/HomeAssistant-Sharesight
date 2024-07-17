435e3c5781d7cd9769084fa1e7d954940ceffabd
Joshua Leaper <poshernater@outlook.com>
2024-07-17 19:27:39 +0930

    Updated to support SensorDeviceClass

--- custom_components/sharesight/sensor.py
+++ custom_components/sharesight/sensor.py
@@ -1,4 +1,11 @@
 from homeassistant.helpers.entity import Entity
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+
+from homeassistant.const import CURRENCY_DOLLAR
 from . import DOMAIN
 from .const import PORTFOLIO_ID, API_VERSION
 import logging
@@ -14,9 +21,9 @@ async def async_setup_entry(hass, entry, async_add_entities):
 class SharesightSensor(Entity):
     def __init__(self, sharesight):
         self._sharesight = sharesight
-        self._state = 0
-        self._name = f"{PORTFOLIO_ID} Portfolio Value "
-        self._unique_id = f"{PORTFOLIO_ID}_portfolio_value"  # Ensure unique_id is unique to this sensor
+        self._state = None
+        self._name = f"{PORTFOLIO_ID} Portfolio Value"
+        self._unique_id = f"{PORTFOLIO_ID}_portfolio_value"
 
     @property
     def name(self):
@@ -30,6 +37,14 @@ class SharesightSensor(Entity):
     def unique_id(self):
         return self._unique_id
 
+    @property
+    def unit_of_measurement(self):
+        return CURRENCY_DOLLAR
+
+    @property
+    def device_class(self):
+        return SensorDeviceClass.MONETARY
+
     async def async_update(self):
         _LOGGER.info(f"CALLING DATA")
         access_token = await self._sharesight.validate_token()
@@ -38,8 +53,11 @@ class SharesightSensor(Entity):
         data = await self._sharesight.get_api_request(f"portfolios/{PORTFOLIO_ID}/valuation", API_VERSION, access_token)
         _LOGGER.info(f"DATA IS {data}")
         if data:
-            port_value = data["value"]
-            _LOGGER.info(f"VALUE IS: {port_value}")
-            self._state = port_value
+            port_value = data.get("value")
+            if port_value is not None:
+                self._state = float(port_value)
+                _LOGGER.info(f"VALUE IS: {self._state}")
+            else:
+                _LOGGER.warning("Value is None")
         else:
             _LOGGER.warning("No data received from Sharesight API")
