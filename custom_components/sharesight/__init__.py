50409d387dbf3c283946c512dfc5fdeba0e83846
Joshua Leaper <poshernater@outlook.com>
2024-07-30 20:57:19 +0930

    Added support for deleting token on service removal

--- custom_components/sharesight/__init__.py
+++ custom_components/sharesight/__init__.py
@@ -39,6 +39,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = local_coordinator
     hass.data.setdefault(DOMAIN, {})["portfolio_id"] = portfolio_id
+    hass.data.setdefault(DOMAIN, {})["sharesight_client"] = client
     entry.async_on_unload(entry.add_update_listener(update_listener))
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
@@ -50,6 +51,15 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
+
+
+async def async_remove_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    _LOGGER.info(f"Removing Sharesight integration: {entry.entry_id}")
+    if entry.entry_id in hass.data[DOMAIN]:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    sharesight = hass.data[DOMAIN]["sharesight_client"]
+    await sharesight.delete_token()
+    return
 
 
 async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
