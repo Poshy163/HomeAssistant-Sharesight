8697f2ed13af613950b94cc7066362da92dcb0eb
Joshua Leaper <poshernater@outlook.com>
2024-09-02 01:22:37 +0930

    Clean up unused code

--- custom_components/sharesight/sensor.py
+++ custom_components/sharesight/sensor.py
@@ -19,7 +19,6 @@ async def async_setup_entry(hass, entry, async_add_entities):
     data = hass.data[DOMAIN][entry.entry_id]
 
     coordinator: SharesightCoordinator = data["coordinator"]
-    sharesight = hass.data[DOMAIN]
     portfolio_id = data["portfolio_id"]
     edge = data["edge"]
     local_currency = coordinator.data['portfolios'][0]['currency_code']
@@ -27,7 +26,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
     sensors = []
 
     for sensor in SENSOR_DESCRIPTIONS:
-        sensors.append(SharesightSensor(sensor, sharesight, entry, coordinator,
+        sensors.append(SharesightSensor(sensor, entry, coordinator,
                                         local_currency, portfolio_id, edge))
 
     __index_market = 0
@@ -35,7 +34,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
         for market_sensor in MARKET_SENSOR_DESCRIPTIONS:
             local_name = market['group_name']
             market_sensor.name = f"{local_name} value"
-            new_sensor = SharesightSensor(market_sensor, sharesight, entry, coordinator,
+            new_sensor = SharesightSensor(market_sensor, entry, coordinator,
                                           local_currency, portfolio_id, edge, __index_market, local_name)
             sensors.append(new_sensor)
             MARKET_SENSORS.append(market_sensor.name)
@@ -46,7 +45,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
         for cash_sensor in CASH_SENSOR_DESCRIPTIONS:
             local_name = cash['name']
             cash_sensor.name = f"{local_name} cash balance"
-            new_sensor = SharesightSensor(cash_sensor, sharesight, entry, coordinator,
+            new_sensor = SharesightSensor(cash_sensor, entry, coordinator,
                                           local_currency, portfolio_id, edge, __index_cash, local_name)
             sensors.append(new_sensor)
             CASH_SENSORS.append(cash_sensor.name)
@@ -65,7 +64,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
                 update_market_sensor.name = f"{__local_name} value"
                 if update_market_sensor.name not in MARKET_SENSORS:
                     local_market_currency = coordinator.data['portfolios'][0]['currency_code']
-                    update_new_sensor = SharesightSensor(update_market_sensor, sharesight, entry, update_coordinator,
+                    update_new_sensor = SharesightSensor(update_market_sensor, entry, update_coordinator,
                                                          local_market_currency, portfolio_id, edge,
                                                          __update_index_market, __local_name)
                     async_add_entities([update_new_sensor], True)
@@ -80,7 +79,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
                 update_cash_sensor.name = f"{__local_name} cash balance"
                 if update_cash_sensor.name not in CASH_SENSORS:
                     local_cash_currency = coordinator.data['portfolios'][0]['currency_code']
-                    update_new_sensor = SharesightSensor(update_cash_sensor, sharesight, entry, update_coordinator,
+                    update_new_sensor = SharesightSensor(update_cash_sensor, entry, update_coordinator,
                                                          local_cash_currency, portfolio_id, edge, __update_index_cash,
                                                          __local_name)
                     CASH_SENSORS.append(update_cash_sensor.name)
@@ -91,7 +90,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
 
 
 class SharesightSensor(CoordinatorEntity, Entity):
-    def __init__(self, sensor, sharesight, entry, coordinator, currency, portfolio_id, edge, index=0, local_name=""):
+    def __init__(self, sensor, entry, coordinator, currency, portfolio_id, edge, index=0, local_name=""):
         super().__init__(coordinator)
         self._state_class = sensor.state_class
         self._coordinator = coordinator
@@ -99,48 +98,22 @@ class SharesightSensor(CoordinatorEntity, Entity):
         self._entity_category = sensor.entity_category
         self._name = str(sensor.name)
         self._index = index
-        self._independent_name = local_name
-        self._edge = edge
         self._suggested_display_precision = sensor.suggested_display_precision
         self._key = sensor.key
         self._icon = sensor.icon
-        self.datapoint = []
         self._entry = entry
-        self._sharesight = sharesight
         self._device_class = sensor.device_class
-        self.currency = currency
         self._sub_key = sensor.sub_key
 
         if sensor.native_unit_of_measurement == CURRENCY_DOLLAR:
-            self._native_unit_of_measurement = self.currency
+            self._native_unit_of_measurement = currency
         else:
             self._native_unit_of_measurement = sensor.native_unit_of_measurement
 
-        entity_type = "sensor"
         base_entity_id = f"{self._name.lower().replace(' ', '_')}_{self._portfolioID}"
+        self.entity_id = f"sensor.{base_entity_id}"
 
-        try:
-            if self._sub_key == "report" and self._key != "sub_totals" or self._sub_key == "report" and self._key != "cash_accounts":
-                self.datapoint.append(self._key)
-                self._state = self._coordinator.data[self._sub_key][self._key]
-                self._unique_id = f"{self._portfolioID}_{self._key}_{APP_VERSION}"
-            elif self._key == "user_id":
-                self.datapoint.append(self._key)
-                self._state = self._coordinator.data[self._sub_key][0][self._key]
-                self._unique_id = f"{self._portfolioID}_{self._key}_{APP_VERSION}"
-            elif "sub_totals" in self._key or "cash_accounts" in self._key:
-                self._state = self._coordinator.data['report'][self._key][self._index][self._sub_key]
-                self._unique_id = f"{self._portfolioID}_{self._independent_name}VALUE_{APP_VERSION}"
-            self.entity_id = f"{entity_type}.{base_entity_id}"
-
-        except ValueError as e:
-            _LOGGER.error(f"KeyError accessing data for key '{self._key}': {e}")
-            self._state = None
-        except IndexError as e:
-            _LOGGER.error(f"IndexError accessing data for key '{self._key}': {e}")
-            self._state = None
-
-        if self._edge:
+        if edge:
             edge_name = " Edge "
         else:
             edge_name = " "
@@ -151,6 +124,25 @@ class SharesightSensor(CoordinatorEntity, Entity):
             configuration_url=f"https://edge-portfolio.sharesight.com/portfolios/{self._portfolioID}",
             model=f"Sharesight{edge_name}API",
             name=f"Sharesight{edge_name}Portfolio {self._portfolioID}")
+
+        try:
+            if self._sub_key == "report" and self._key != "sub_totals" or self._sub_key == "report" and self._key != "cash_accounts":
+
+                self._state = self._coordinator.data[self._sub_key][self._key]
+                self._unique_id = f"{self._portfolioID}_{self._key}_{APP_VERSION}"
+            elif self._key == "user_id":
+                self._state = self._coordinator.data[self._sub_key][0][self._key]
+                self._unique_id = f"{self._portfolioID}_{self._key}_{APP_VERSION}"
+            elif "sub_totals" in self._key or "cash_accounts" in self._key:
+                self._state = self._coordinator.data['report'][self._key][self._index][self._sub_key]
+                self._unique_id = f"{self._portfolioID}_{local_name}_VALUE_{APP_VERSION}"
+
+        except ValueError as e:
+            _LOGGER.error(f"KeyError accessing data for key '{self._key}': {e}")
+            self._state = None
+        except IndexError as e:
+            _LOGGER.error(f"IndexError accessing data for key '{self._key}': {e}")
+            self._state = None
 
     @callback
     def _handle_coordinator_update(self):
